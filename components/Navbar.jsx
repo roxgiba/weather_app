8e683d4a2bd693dce5bf55c5b88ac5a8eda4e764
roxgiba <rox.giba@gmail.com>
2024-01-22 10:33:57 +0100

    navbar created

---
+++ components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>WEATHER APP</h1>
+      <h3>NextJS project for personal portfolio</h3>
+      <form className="search">
+        <label for="src">
+          Search:
+          <input
+            id="src"
+            type="search"
+            name="src"
+            placeholder="Enter your city..."
+          />
+        </label>
+      </form>
+    </div>
+  );
+}
